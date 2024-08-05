9d341b0e9c87f960b69d0470677ba85431ad19f2
robin-ivi <mishrarabin18@gmail.com>
2024-08-05 23:31:14 +0530

    Bootstrao and props

--- first/src/App.js
+++ first/src/App.js
@@ -1,24 +1,83 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Header } from './Header';
+import Footer from './Footer';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function App() {
   let a = "Krishna";
   let obj = {
-    'name' : "Krishna",
-    'class' : "BA"
+    'name': "Krishna",
+    'class': "BA"
   }
   let j = true;
   return (
     <div className="main">
-     { a }
-     {obj.class}
-    { (j) ?
-    <h1 style={{ color:"red",backgroundColor:"yellow"}}>ok</h1>
-     :
-     ""
-     }
-
+      <Header email="wsa@gmail.com" asd={obj} />
+      <Container fluid>
+        <Row>
+          <Col className='col-12 text-center py-4'>
+            <h1>One Demo</h1>
+          </Col>
+          <Col lg="3" md="6">
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg="3" md="6">
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg="3" md="6">
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg="3" md="6">
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+      <Footer />
     </div>
   );
 }
---
+++ first/src/Header.jsx
@@ -0,0 +1,8 @@
+function Header(props) {
+    console.log(props)
+    return (
+        <h1>Welcome To RentalWheels {props.asd.name}</h1>
+    )
+}
+
+export {Header};
